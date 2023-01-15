c87ad4001f2dd6e90a7bf6dcd8d3070b461bf7ad
Tomara Petty <tomararuth@gmail.com>
2023-01-15 12:33:38 -0800

    put fetchTrips into a callback to prevent checking a reference of the state which causes and infinit loop

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 // styles
 import './TripList.css';
 
@@ -6,11 +6,15 @@ export default function TripList() {
   const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState('http://localhost:3000/trips');
 
+  const fetchTrips = useCallback(async () => {
+    const response = await fetch(url)
+    const json = await response.json()
+    setTrips(json)
+  }, [url])
+
   useEffect(() => {
-    fetch(url)
-      .then(response => response.json())
-      .then(json => setTrips(json));
-  }, [url]);
+    fetchTrips()
+  }, [fetchTrips]);
 
   console.log(trips);
 
